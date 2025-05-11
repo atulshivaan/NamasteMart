4a9da2109b45de292f130c98890b409facd12e61
Atul Shivaan <atulshivaan@gmail.com>
2025-05-11 14:19:35 +0530

    middleware added

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,18 +1,19 @@
 import express from "express"
 import { forgotPassword, getUserbyID, getUsers, resetPasswordWithCode, updatePassword, userDelete, userEdit, userLogin, userLogout, userRegister, verifyUser } from "../controllers/user.controller.js";
+import authenticateUser from "../middleware/authenticateUser.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/register",userRegister)
 userRouter.post("/verify-user",verifyUser)
 userRouter.post("/login",userLogin)
-userRouter.get("/get-users" , getUsers)
-userRouter.get("/get-user/:id",getUserbyID)
-userRouter.put("/edit-user/:id",userEdit)
-userRouter.delete("/delete-user/:id",userDelete)
-userRouter.post("/forgot-password", forgotPassword);
-userRouter.post("/reset-password", resetPasswordWithCode);
-userRouter.put("/update-password", updatePassword);
+userRouter.get("/get-users" , authenticateUser, getUsers)
+userRouter.get("/get-user/:id", authenticateUser,getUserbyID)
+userRouter.put("/edit-user/:id", authenticateUser,userEdit)
+userRouter.delete("/delete-user/:id", authenticateUser, userDelete)
+userRouter.post("/forgot-password",authenticateUser, forgotPassword);
+userRouter.post("/reset-password",authenticateUser, resetPasswordWithCode);
+userRouter.put("/update-password",authenticateUser, updatePassword);
 userRouter.post("/logout",userLogout)
 
 
