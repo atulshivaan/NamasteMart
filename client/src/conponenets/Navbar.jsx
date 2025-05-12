0f5218df5f582a75cf6d92244b0c48d0b80f58b0
Atul Shivaan <atulshivaan@gmail.com>
2025-05-12 21:31:20 +0530

    user verification

--- client/src/conponenets/Navbar.jsx
+++ client/src/conponenets/Navbar.jsx
@@ -1,6 +1,30 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { FiLogOut } from "react-icons/fi";
+import axiosInstance from "../utils/axiosInstance";
+ 
 
 const Navbar = () => {
+  const navigate = useNavigate();  
+
+ 
+  const handleLogout = async () => {
+    try {
+      
+      const response = await axiosInstance.post("/api/v1/admin/logout");
+
+      if (response.status === 200) {
+        // Clear authentication token from localStorage or cookies
+        localStorage.removeItem("token");
+
+       
+        navigate("/login");
+      }
+    } catch (error) {
+      console.error("Logout failed:", error);
+      alert("An error occurred while logging out. Please try again.");
+    }
+  };
+
   return (
     <div className="flex items-center justify-between text-black h-15 m-2 w-full shadow-2xl px-8 rounded-md">
       {/* Left - Heading */}
@@ -15,7 +39,8 @@ const Navbar = () => {
         <Link to="/products">Products</Link>
       </div>
 
-      
+      {/* Logout Button */}
+      <FiLogOut onClick={handleLogout} className="cursor-pointer" />
     </div>
   );
 };
